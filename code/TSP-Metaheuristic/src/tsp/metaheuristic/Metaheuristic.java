b98de5c74658da91e629b88693bf9587e39a9159
Ronaldo Silveira <ronaldof1s1@gmail.com>
2015-06-04 11:50:02 -0300

    'finished' tabu search

--- code/TSP-Metaheuristic/src/tsp/metaheuristic/Metaheuristic.java
+++ code/TSP-Metaheuristic/src/tsp/metaheuristic/Metaheuristic.java
@@ -13,10 +13,43 @@ import java.util.ArrayList;
  */
 public class Metaheuristic
 {
-  Integer[][] neighborhood(Integer[][] graph, Integer[] solution, ArrayList<Integer[]> tabuTable)
+  Integer[] reverse(Integer[] list)
   {
-    Integer[][] neighborhood;
-    neighborhood = new Integer[1][solution.length];
+    Integer[] temp;
+    temp = new Integer[0];
+    for(int i = 0; i < list.length/2; i++)
+    {
+      temp = swapNodes(list, i, list.length - i);
+    }
+    return temp;
+  }
+  Integer[] swapNodes(Integer[] list, int index1, int index2)
+  {
+    int temp = list[index1];
+    list[index1] = list[index2];
+    list[index2] = temp;
+    return list;
+  }
+  
+  ArrayList neighborhood(Integer[][] graph, Integer[] solution, ArrayList tabuTable)
+  {
+    ArrayList<Integer[]> neighborhood;
+    neighborhood = new ArrayList<>();
+    
+    Integer[] move, tempSol;
+    move = new Integer[2];
+    Integer node = 0;
+    move[0] = node;
+    for(int i = 0; i < solution.length; )
+    {
+      move[1] = i;
+      if(!(tabuTable.contains(move) && tabuTable.contains(reverse(move))))
+      {
+        tempSol = swapNodes(solution, move[1], move[2]);
+        neighborhood.add(tempSol);
+        tabuTable.add(move);
+      }
+    }
     return neighborhood;
   }
   
@@ -31,10 +64,31 @@ public class Metaheuristic
   {
     ArrayList<Integer[]> table;
     table = new ArrayList<>();
+    ArrayList<Integer[]> neighbors1, neighbors2;
     
+    Integer currObj, minObj;
+    
+    Integer[] bestSol;
+    bestSol = solution;
+    
+    currObj = objective(graph, solution);
+    minObj = currObj;
     for(int i = 0; i < solution.length; i++)
     {
-      
+      neighbors1 = neighborhood(graph, bestSol, table);
+      for (Integer[] neighbor1 : neighbors1)
+      {
+        neighbors2 = neighborhood(graph, neighbor1, table);
+        for(Integer[] neighbor2 : neighbors2)
+        {
+          currObj = objective(graph, neighbor2);
+          if(currObj < minObj)
+          {
+            minObj = currObj;
+            bestSol = neighbor2;
+          }
+        }
+      }
     }
   }
 }
